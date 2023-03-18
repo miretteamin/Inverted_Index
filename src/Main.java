916e7ed2502465378347921cd671cac38f6a77a7
ChristinaMontasser <chrismontasser14@gmail.com>
2023-03-18 22:09:21 +0200

    v1.1
    Read Files

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
-public class Main {
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.ArrayList; 
 
+public class Main {
+	
+	static String [] filesContent = new String[10]; 
+	static void read_files() {
+	    try {
+    	 File path = new File(System.getProperty("user.dir")+"\\Documents");
+    	 File [] files = path.listFiles();
+    	    for (int i = 0; i < files.length; i++){
+    	        if (files[i].isFile()){ //this line weeds out other directories/folders
+    		      Scanner myReader = new Scanner(files[i]);
+    		      //System.out.println(i);
+    		      //String data;
+    		      while (myReader.hasNextLine()) {
+    		    	 filesContent[i] = myReader.nextLine();
+    		      //System.out.println(data);
+    		      }
+    		      myReader.close();
+    	        }
+    	    }
+	    } catch (FileNotFoundException e) {
+	      System.out.println("An error occurred.");
+	      e.printStackTrace();
+	    }
+	   //System.out.println(filesContent[0]);
+	  }
     public static void main(String[] args) {
-	// write your code here
+    	read_files();
     }
 }
---
+++ src/Posting.java
@@ -0,0 +1,6 @@
+
+public class Posting {
+	/*public Posting next = null;
+	 int docId;
+	 int dtf = 1; // document term frequency*/
+}
